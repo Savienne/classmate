9e7a397b77c7ee7ecfde83263b13d903e2924f50
Savienne <Saviennes.mitchell@yahoo.com>
2022-07-26 17:44:29 -0400

    adjusting background photo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,14 @@
 import styles from './Home.module.css'
 import { useState } from 'react'
 import { validateFormCollection } from "../../services/profileService"
-
-// MUI Components
 import Quote from '../../components/Quote/Quote'
 import Login from '../../pages/Login/Login'
 import Grid from "@mui/material/Grid"
 import { Typography } from "@mui/material";
-import Tooltip from '@mui/material/Tooltip';
-import Button from '@mui/material/Button';
-import LogoutIcon from '@mui/icons-material/Logout';
-
 // Components
 import AddTaskForm from '../../components/AddTaskForm/AddTaskForm'
 import TaskList from '../../components/TaskList/TaskList'
 import DeleteAllDialog from "../../components/Home/DeleteAllDialog"
-import ReactYouTube from '../../components/ReactYouTube/ReactYouTube'
 
 const Home = ({ 
   user, 
@@ -24,8 +17,7 @@ const Home = ({
   handleDeleteTask,
   handleEditTask,
   handleDeleteAll,
-  handleSignupOrLogin,
-  handleLogout
+  handleSignupOrLogin
 }) => {
 
   const [edit, setEdit] = useState(false)
@@ -82,48 +74,38 @@ const Home = ({
   return (
     <>
       {user ? 
-        <Grid container sx={{
-          width: '100%', 
-          bgcolor: 'background.paper', 
-          color: 'primary'
-        }}>
-          <Grid item>
-            <main className={styles.container}>
-              <Typography 
-                variant='h1' 
-                sx={{color: "white", fontStyle: "italic", fontWeight: 700}}
-              >
-                Welcome, {user ? user.name : 'friend'}
-              </Typography>
-              <Tooltip title="Logout">
-                <Button onClick={handleLogout}>
-                  <LogoutIcon sx={{color: "white"}}/>
-                </Button>
-              </Tooltip>
-              <Quote />
-              <AddTaskForm 
-                formData={formData}
-                errors={errors}
-                edit={edit}
-                handleChange={handleChange}
-                handleSubmit={handleSubmit}
-                checkValidForm={checkValidForm}
-                handleClearForm={handleClearForm}
-              />
-              <TaskList 
-                tasks={tasks} 
-                handleDeleteTask={handleDeleteTask}
-                handleEditTaskButton={handleEditTaskButton}
-              />
-              <DeleteAllDialog handleDeleteAll={handleDeleteAll}/>
-              <ReactYouTube videoId={"jfKfPfyJRdk"}/>
-            </main>
-          </Grid>
-        </Grid>
+      <Grid container sx={{ width: '100%', 
+      // bgcolor: 
+      // 'background.paper', color: 'primary',
+    }}
+      >
+        <Grid item>
+        <main className={styles.container}>
+          <Typography variant='h1' sx={{color: "#67c1f5"}}>Welcome, {user ? user.name : 'friend'}</Typography>
+          <Quote />
+          <AddTaskForm 
+            formData={formData}
+            errors={errors}
+            edit={edit}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+            checkValidForm={checkValidForm}
+            handleClearForm={handleClearForm}
+          />
+          <TaskList 
+            tasks={tasks} 
+            handleDeleteTask={handleDeleteTask}
+            handleEditTaskButton={handleEditTaskButton}
+          />
+          <DeleteAllDialog handleDeleteAll={handleDeleteAll}/>
+        </main>
+      </Grid>
+      </Grid>
         :
         <main>
           <Login handleSignupOrLogin={handleSignupOrLogin}/>
         </main>
+
       }
     </>
   )
