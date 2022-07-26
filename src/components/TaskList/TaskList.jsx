94aad3533b24ecd5df581cbd910857c0069b4417
Savienne <Saviennes.mitchell@yahoo.com>
2022-07-26 10:25:00 -0400

    work on styling

---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Divider from "@mui/material/Divider";
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import EditIcon from '@mui/icons-material/Edit';
+import Tooltip from '@mui/material/Tooltip';
+
+// Components
+import DeleteDialog from './DeleteDialog';
+
+export default function TaskList({
+  tasks, 
+  handleDeleteTask, 
+  handleEditTaskButton}) 
+{
+  return (
+    <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+      {tasks.map(task => 
+        <ListItem key={task._id}>
+          <ListItemAvatar>
+            <Avatar>
+              <AssignmentIcon />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText 
+            primary={task.name} 
+            secondary={task.content}
+          />
+          <Divider sx={{ height: 28, m: 0.5, bgcolor: "grey.800" }} orientation="vertical" />
+          <ListItemText 
+            primary={task.priority} 
+          />
+          <DeleteDialog 
+            task={task} 
+            handleDeleteTask={handleDeleteTask}
+          />
+          <Tooltip title="Edit">
+            <Button
+              onClick={() => handleEditTaskButton(task._id)}
+            >
+              <EditIcon/>
+            </Button>
+          </Tooltip>
+        </ListItem>
+      )}
+    </List>
+  );
+}
